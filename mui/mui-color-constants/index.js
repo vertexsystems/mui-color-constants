e992d1561380380c51223b0cbe4e99c0dead03f0
Chedwick Montoril <chedwick@gmail.com>
2017-11-04 01:07:49 -0700

    Rename rgba to alpha-channel

--- mui/mui-color-constants/index.js
+++ mui/mui-color-constants/index.js
@@ -1,4 +1,4 @@
+export * as AlphaChannel from './alpha-channel';
 export * as CssColor from './w3c/css-named-colors';
 export * as HtmlColor from './w3c/html-named-colors';
 export * as MaterialDesignColor from './material-design';
-export * as RgbaColor from './rgba';
