6b98557bb50fe4652afcfafbcabf7b6b89faaa66
Chedwick Montoril <chedwick@gmail.com>
2017-11-04 01:03:48 -0700

    Fix import/export issue

--- mui/mui-color-constants/index.js
+++ mui/mui-color-constants/index.js
@@ -1,3 +1,4 @@
-export * from './material-design';
-export * from './rgba';
-export * from './w3c';
+export * as CssColor from './w3c/css-named-colors';
+export * as HtmlColor from './w3c/html-named-colors';
+export * as MaterialDesignColor from './material-design';
+export * as RgbaColor from './rgba';
--- mui/mui-color-constants/w3c/index.js
+++ mui/mui-color-constants/w3c/index.js
@@ -1,2 +1,1 @@
 export * from './css-named-colors';
-export * from './html-named-colors';
