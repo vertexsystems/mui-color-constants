254bd6f54cc0d20a3b255ba357bacf38a5c2e53c
Chedwick Montoril <chedwick@gmail.com>
2017-11-07 21:44:52 -0800

    Refactor Material UI color import/export

--- mui/mui-color-constants/material-design/index.js
+++ mui/mui-color-constants/material-design/index.js
@@ -1,21 +1,47 @@
-export * from './amber';
-export * from './black';
-export * from './blue-grey';
-export * from './blue';
-export * from './brown';
-export * from './cyan';
-export * from './deep-orange';
-export * from './deep-purple';
-export * from './green';
-export * from './grey';
-export * from './indigo';
-export * from './light-blue';
-export * from './light-green';
-export * from './lime';
-export * from './orange';
-export * from './pink';
-export * from './purple';
-export * from './red';
-export * from './teal';
-export * from './white';
-export * from './yellow';
+import * as amber from './amber';
+import * as black from './black';
+import * as blueGrey from './blue-grey';
+import * as blue from './blue';
+import * as brown from './brown';
+import * as cyan from './cyan';
+import * as deepOrange from './deep-orange';
+import * as deepPurple from './deep-purple';
+import * as green from './green';
+import * as grey from './grey';
+import * as indigo from './indigo';
+import * as lightBlue from './light-blue';
+import * as lightGreen from './light-green';
+import * as lime from './lime';
+import * as orange from './orange';
+import * as pink from './pink';
+import * as purple from './purple';
+import * as red from './red';
+import * as teal from './teal';
+import * as white from './white';
+import * as yellow from './yellow';
+
+export const MaterialDesignColor = {
+  ...amber,
+  ...black,
+  ...blueGrey,
+  ...blue,
+  ...brown,
+  ...cyan,
+  ...deepOrange,
+  ...deepPurple,
+  ...green,
+  ...grey,
+  ...indigo,
+  ...lightBlue,
+  ...lightGreen,
+  ...lime,
+  ...orange,
+  ...pink,
+  ...purple,
+  ...red,
+  ...teal,
+  ...white,
+  ...yellow
+};
+
+export default MaterialDesignColor;
